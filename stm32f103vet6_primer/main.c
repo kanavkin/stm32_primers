333b0a7c05be8289e6c18c251d956cdca5f2f76b
Vladimir Kanavkin <vkanavkin@bk.ru>
2019-02-10 23:07:34 +0100

    cleaned up stm32f103_vet6 primer

--- stm32f103vet6_primer/main.c
+++ stm32f103vet6_primer/main.c
@@ -2,67 +2,51 @@
  * stm32 timer led example main.c
  ********************************/
 
-#include "stm32_libs/stm32f10x/spl/stm32f10x.h"
-#include "stm32_libs/stm32f10x/boctok/stm32f10x_gpio_boctok.h"
 #include "stm32_libs/boctok_types.h"
+#include "stm32_libs/stm32f10x/stm32f10x.h"
+#include "stm32_libs/stm32f10x/boctok/stm32f10x_gpio_boctok.h"
 
-
-/* User defined function prototypes */
-void led_toggle(void);
 
 
 int main(void)
 {
-  /* Enable PORT C clock */
+  // Enable PORT C clock
   RCC->APB2ENR |= RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPAEN;
 
-  /* Initialize GPIOC PIN13 */
+  // Enable TIM2 clock, PCLK1= 64 MHz
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+  
+  // Initialize GPIOC PIN13
   GPIO_configure(GPIOC, 13, GPIO_OUT_OD_2MHZ);
 
-  /* Turn off LED to start with */
+  // Turn off LED to start with
   GPIOC->BSRR= (uint32_t) (1 << 13);
   
-  /* Enable TIM2 clock, PCLK1= 64 MHz */
-  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+  //enable timer 3 irq (prio 2)
+  NVIC_SetPriority(TIM2_IRQn, 1UL);
+  NVIC_ClearPendingIRQ(TIM2_IRQn);
+  NVIC_EnableIRQ(TIM2_IRQn);
 
-  /* Enable TIM2 global interrupt */
-  NVIC->ISER[0] |= 0x10000000;
-
-  /* Frequency after prescaler = 32MHz/(31999+1) = 1KHz.
-   * Compare register = 500 so a compare match occurs every 0.5s -> T= 0,1 ms
-   */
+  /*
+  Frequency after prescaler = 64MHz/(63999+1) = 1KHz.
+  Compare register = 500 so a compare match occurs every 0.5s -> T= 0,1 ms
+  */
   TIM2->PSC = (uint16_t) 63999;
   TIM2->CCR1 = (uint16_t) 500;
 
+  //generate update event to write new prescaler and compare value
   TIM2->EGR = TIM_EGR_UG;
 
-  
-  /* Enable Capture/Compare 1 interrupt */
+  // enable Capture/Compare 1 interrupt
   TIM2->DIER |= (uint16_t)0x0002;
 
-  /* Enable TIM2 counter (in upcount mode) */
+  // enable TIM2 counter (in upcount mode)
   TIM2->CR1 |= (uint16_t)0x0001;
-
-  // DEBUG enable MCO (hse)
-  //  GPIO_configure(GPIOA, 8, GPIO_AF_PP_50MHZ);
-  //   RCC->CFGR |= RCC_CFGR_MCO_SYSCLK;
   
-   U32 delay;
 
-  
   while(1)
     {
-       /*
-	26720 gives 40 ms @ 8MHz (-> 12 cycles/ NOP) (system_stm32f10x_hsi)
-	or 5,193 ms @ 8 MHz (-> 1,77 cycles / NOP) (system_stm32f10x.c)
-      */
-      /*
-      for(delay=0; delay < 26720; delay++)
-	{
-	  asm volatile ("nop");
-	}
-        gpio_set_pin(GPIOC, 13, TOGGLE);
-      */
+
     }
 }
 
@@ -70,19 +54,19 @@ int main(void)
 /* Timer 2 Interrupt Service Routine */
 void TIM2_IRQHandler(void)
 {
-  /* Toggle LED if TIM2's Capture/Compare 1 interrupt occurred. 
-  if(TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET)
+  if( TIM2->SR & TIM_SR_CC1IF)
     {
-      TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
+      //clear irq pending bit
+      TIM2->SR= (U16) ~TIM_SR_CC1IF;
 
-      led_toggle();
+      //reset counter
+      TIM2->CNT= (U16) 0;
+
+      /**
+      toggle led
+      */
+      gpio_set_pin(GPIOC, 13, TOGGLE);
+
     }
-  */
-
-  TIM2->CNT= (uint16_t) 0;
-  TIM2->SR= (uint16_t) 0;
-
-  //led_toggle();
-  gpio_set_pin(GPIOC, 13, TOGGLE);
 }
 
